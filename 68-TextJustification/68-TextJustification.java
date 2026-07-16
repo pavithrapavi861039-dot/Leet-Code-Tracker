d2b591b65678806cc3d0dfdc41d2786663cd8b6f
Pavithra R <pavithrapavi861039@gmail.com>
2026-07-16 16:12:46 +0530

    Create 68-TextJustification

---
+++ 68-TextJustification/68-TextJustification.java
@@ -0,0 +1,44 @@
+// Last updated: 7/16/2026, 4:12:46 PM
+import java.util.*;
+class Solution {
+    public List<String> fullJustify(String[] words, int maxWidth) {
+        List<String> result = new ArrayList<>();
+        int i = 0;
+        while (i < words.length) {
+            int j = i;
+            int lineLength = 0;
+            while (j < words.length && 
+                  lineLength + words[j].length() + (j - i) <= maxWidth) {
+                lineLength += words[j].length();
+                j++;
+            }
+            int gaps = j - i - 1;
+            StringBuilder line = new StringBuilder();
+            if (j == words.length || gaps == 0) {
+                for (int k = i; k < j; k++) {
+                    line.append(words[k]);
+                    if (k < j - 1) line.append(" ");
+                }
+                while (line.length() < maxWidth) {
+                    line.append(" ");
+                }
+            } else {
+                int totalSpaces = maxWidth - lineLength;
+                int evenSpaces = totalSpaces / gaps;
+                int extraSpaces = totalSpaces % gaps;      
+                for (int k = i; k < j; k++) {
+                    line.append(words[k]);
+                    if (k < j - 1) {
+                        int spaces = evenSpaces + (k - i < extraSpaces ? 1 : 0);
+                        for (int s = 0; s < spaces; s++) {
+                            line.append(" ");
+                        }
+                    }
+                }
+            }
+            result.add(line.toString());
+            i = j;
+        }
+        return result;
+    }
+}
