a0c29d22eb0a5816624e6c910b3b01f0da95b087
Pavithra R <pavithrapavi861039@gmail.com>
2026-07-16 16:06:04 +0530

    Create 637-AverageOfLevelsInBinaryTree

---
+++ 637-AverageOfLevelsInBinaryTree/637-AverageOfLevelsInBinaryTree.java
@@ -0,0 +1,22 @@
+// Last updated: 7/16/2026, 4:06:04 PM
+import java.util.*;
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> result = new ArrayList<>();
+        if (root == null) return result;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            int size = q.size();
+            double sum = 0;
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                sum += node.val;
+                if (node.left != null) q.add(node.left);
+                if (node.right != null) q.add(node.right);
+            }
+            result.add(sum / size); 
+        }
+        return result;
+    }
+}
