c4740a1773d2588950d8c13d073d03799661320e
Pavithra R <pavithrapavi861039@gmail.com>
2026-07-16 16:07:50 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/283-MoveZeroes.java
@@ -0,0 +1,16 @@
+// Last updated: 7/16/2026, 4:07:50 PM
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        while (index < nums.length) {
+            nums[index] = 0;
+            index++;
+        }
+    }
+}
