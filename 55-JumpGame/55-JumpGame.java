0401c526cd87abbee05f5c4d71ca22ec351b16c0
Pavithra R <pavithrapavi861039@gmail.com>
2026-07-16 16:13:02 +0530

    Create 55-JumpGame

---
+++ 55-JumpGame/55-JumpGame.java
@@ -0,0 +1,12 @@
+// Last updated: 7/16/2026, 4:13:01 PM
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxReach = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > maxReach) return false;
+            maxReach = Math.max(maxReach, i + nums[i]);
+            if (maxReach >= nums.length - 1) return true;
+        }
+        return true;
+    }
+}
