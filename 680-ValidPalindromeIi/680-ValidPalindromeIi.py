8533c617c7d024f8af310a08bb8d1168242872d5
Pavithra R <pavithrapavi861039@gmail.com>
2026-07-25 09:56:02 +0530

    Create 680-ValidPalindromeIi

---
+++ 680-ValidPalindromeIi/680-ValidPalindromeIi.py
@@ -0,0 +1,18 @@
+# Last updated: 7/25/2026, 9:56:02 AM
+class Solution:
+    def validPalindrome(self, s):
+        def is_palindrome(l, r):
+            while l < r:
+                if s[l] != s[r]:
+                    return False
+                l += 1
+                r -= 1
+            return True
+        left, right = 0, len(s) - 1
+        while left < right:
+            if s[left] != s[right]:
+                return is_palindrome(left + 1, right) or is_palindrome(left, right - 1)
+            left += 1
+            right -= 1
+        return True
+        
