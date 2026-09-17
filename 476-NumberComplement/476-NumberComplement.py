8ae697c3f2076bde07a81d2a5324919bdcf6c600
Pavithra R <pavithrapavi861039@gmail.com>
2026-09-17 10:18:51 +0530

    File updated at 9/17/2026, 10:18:49 AM

--- 476-NumberComplement/476-NumberComplement.py
+++ 476-NumberComplement/476-NumberComplement.py
@@ -1,6 +1,11 @@
-# Last updated: 9/17/2026, 10:17:35 AM
+# Last updated: 9/17/2026, 10:18:49 AM
 1class Solution:
 2    def findComplement(self, num):
-3        bits = num.bit_length()
-4        mask = (1 << bits) - 1
-5        return num ^ mask
+3        binary = bin(num)[2:]
+4        complement = ""
+5        for bit in binary:
+6            if bit == "0":
+7                complement += "1"
+8            else:
+9                complement += "0"
+10        return int(complement, 2)
